b02e0266122a8f97dbf23271826053157958b0a2
HaiNguyen <nvhai.work.vn@gmail.com>
2015-05-19 09:52:25 +0700

    Uncomment crash report code

--- eulou/src/main/java/com/ezzet/eulou/EolouApplication.java
+++ eulou/src/main/java/com/ezzet/eulou/EolouApplication.java
@@ -9,10 +9,10 @@ import android.app.Application;
 
 import com.facebook.FacebookSdk;
 
-//@ReportsCrashes(formKey = "", mailTo = "nvhai.work.vn@gmail.com", customReportContent = {
-//		ReportField.APP_VERSION_CODE, ReportField.APP_VERSION_NAME,
-//		ReportField.ANDROID_VERSION, ReportField.PHONE_MODEL,
-//		ReportField.CUSTOM_DATA, ReportField.STACK_TRACE, ReportField.LOGCAT}, mode = ReportingInteractionMode.TOAST, resToastText = R.string.crash_toast_text)
+@ReportsCrashes(formKey = "", mailTo = "nvhai.work.vn@gmail.com", customReportContent = {
+		ReportField.APP_VERSION_CODE, ReportField.APP_VERSION_NAME,
+		ReportField.ANDROID_VERSION, ReportField.PHONE_MODEL,
+		ReportField.CUSTOM_DATA, ReportField.STACK_TRACE, ReportField.LOGCAT}, mode = ReportingInteractionMode.TOAST, resToastText = R.string.crash_toast_text)
 public class EolouApplication extends Application {
 
 	@Override
@@ -20,6 +20,6 @@ public class EolouApplication extends Application {
 		// TODO Auto-generated method stub
 		super.onCreate();
 		FacebookSdk.sdkInitialize(getApplicationContext());
-		// ACRA.init(this);
+		ACRA.init(this);
 	}
 }
